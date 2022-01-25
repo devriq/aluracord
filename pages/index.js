91b581083a9b75069cfc7e94a52de4a3803aaac9
devriq <devriq.cordovil@gmail.com>
2022-01-25 11:57:17 -0300

    chat router added

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,7 @@
 import appConfig from "../config.json";
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
 import { useState } from "react";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import { useRouter } from "next/router";
 
 function Titulo(props) {
   const Tag = props.tag || "h1";
@@ -64,10 +34,10 @@ function Titulo(props) {
 
 export default function PaginaInicial() {
   const [username, setUsername] = useState("devriq");
+  const route = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -101,6 +71,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function(e){
+              e.preventDefault();
+              route.push('/chat')
+            }
+            }
             styleSheet={{
               display: "flex",
               flexDirection: "column",
